1ff496bbf48f7d2715b97a28560bd4ae21f0ff93
Mahmoud Fouad <mfapolice@gmail.com>
2023-03-29 05:52:21 +0200

    update

--- 2-how-node-works/starter/event.js
+++ 2-how-node-works/starter/event.js
@@ -1,6 +1,13 @@
 const EventEmitter = require("events");
+const http = require("http");
 
-const myEmitter = new EventEmitter();
+class Sale extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sale();
 
 myEmitter.emit("newSale");
 
@@ -11,3 +18,28 @@ myEmitter.on("mahmoud", (stock) =>
 );
 
 myEmitter.emit("mahmoud", 12);
+
+/////////////////////////////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received");
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  res.writeHead(200, {
+    "Content-type": "text/html",
+  });
+  console.log("Request received");
+  res.end("<h1>Request received ❤</h1>");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen("8000", "127.0.0.1", () => {
+  console.log("server on");
+});
