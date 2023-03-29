969226e6d074b6c17ce3cff0cb20980bb212ebba
Mahmoud Fouad <mfapolice@gmail.com>
2023-03-29 17:21:11 +0200

    update

--- 2-how-node-works/starter/stream.js
+++ 2-how-node-works/starter/stream.js
@@ -3,10 +3,19 @@ const server = require('http').createServer();
 
 server.on('request', (req, res) => {
   // solution one
-  fs.readFile('./test-file.txt', 'utf-8', (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
-  });
+  // fs.readFile('./test-file.txt', 'utf-8', (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+  
+  // solution two: Stream
+  const readable = fs.createReadStream('test-file.txt')
+  readable.on('data', chunk => {
+    res.write(chunk)
+  })
+  readable.on('end', () => {
+    res.end()
+  })
 });
 
 server.listen(8000, 'localhost', () => {
