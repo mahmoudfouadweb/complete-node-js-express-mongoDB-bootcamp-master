995f1a568e033fef5536cfefb7286302d9a03e95
Mahmoud Fouad <mfapolice@gmail.com>
2023-03-30 02:22:19 +0200

    section 2 done

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -1,2 +1,17 @@
-console.log(arguments);
-console.log(require('moduleB'));
+// console.log(arguments);
+// console.log(require('module'));
+
+const calculater = require('./test-module');
+
+const calc1 = new calculater();
+
+// const add = calc1.add(5, 5);
+// console.log(add);
+
+const { add, devide, multiply } = require('./test-module2');
+console.log(add(4, 4));
+
+const hi = require('./test-module3')
+ hi('💥')
+ hi('😂')
+ hi('🤣')
---
+++ 2-how-node-works/starter/test-module3.js
@@ -0,0 +1,3 @@
+console.log('Hello from module I run only ones');
+
+module.exports = (is) => console.log(`hi from module ${is}`);
