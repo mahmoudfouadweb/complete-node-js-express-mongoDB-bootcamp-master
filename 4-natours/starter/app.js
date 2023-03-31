3246f49cadae5e1ba9b9bd02ee46e4dcca485e80
Mahmoud Fouad <mfapolice@gmail.com>
2023-03-31 23:52:37 +0200

    update

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const exp = require('constants');
 
 const app = express();
 
+// 1) MIDDLEWARES third party app
+app.use(express.json());
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello From The Server', app: 'Natours' });
 // });
@@ -14,16 +17,61 @@ const app = express();
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-
 const results = tours.length;
 
+// Handle GET request and make response
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: 'success', 
+    status: 'success',
     results,
-    tours, 
-  })
+    data: {
+      tours
+    }
+  });
 });
+
+// Handle GET request and make response
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      data: {
+        message: 'Invaid Page Number'
+      }
+    });
+  }
+
+  res.status(200).json({
+    status: 'success me',
+    data: {
+      tour
+    }
+  });
+});
+
+// Handle POST request and make response
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+});
+
 const port = 8000;
 app.listen(port, () => {
   console.log(`App Running on part ${port}`);
