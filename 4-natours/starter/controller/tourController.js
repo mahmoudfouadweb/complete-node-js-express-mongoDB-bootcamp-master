37aeed3435e9083a5c335aebaa15c3f939ab23c0
Mahmoud Fouad <mfapolice@gmail.com>
2023-04-03 23:59:52 +0200

    update

--- 4-natours/starter/controller/tourController.js
+++ 4-natours/starter/controller/tourController.js
@@ -83,9 +83,15 @@ exports.updateTour = async (req, res) => {
 exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id);
+
     res.status(204).json({
       status: 'success',
       data: null
     });
-  } catch (err) {}
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
