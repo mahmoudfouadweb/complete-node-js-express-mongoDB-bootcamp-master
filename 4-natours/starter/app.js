dbe4670da8e4109b135f02cca6e6c12eb7516a42
Mahmoud Fouad <mfapolice@gmail.com>
2023-04-10 17:29:42 +0200

    Error Class

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 
 const userRouters = require('./routers/usersRoutes');
 const tourRouters = require('./routers/toursRoutes');
+const AppError = require('./util/appError');
 
 /* -------------------------- Express JS Begin here ------------------------- */
 const app = express();
@@ -13,7 +14,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => next());
 app.use((req, res, next) => {
   req.requistTime = new Date().toISOString();
   next();
@@ -23,16 +23,9 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouters);
 app.use('/api/v1/users', userRouters);
 
-app.all('*', (req, res, next) => {
-  // res.status(404).send({
-  //   staus: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server`
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 404; // set the error status code to 404 (Not Found)
-  err.statusCode = 'fail';
-  next(err);
-});
+app.all('*', (req, res, next) =>
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+);
 
 //3) ERROR HANDLING
 app.use((err, req, res, next) => {
--- 4-natours/starter/util/appError.js
+++ 4-natours/starter/util/appError.js
@@ -2,7 +2,7 @@ class AppError extends Error {
   constructor(message, statusCode) {
     super(message); // Call the "Error" constructor, then pass in the message and statusCode as the arguments. This automatically attaches the prototype from the Error class
     this.statusCode = statusCode;
-    this.status = statusCode.startWith('4') ? 'fail' : 'error';
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true; // This will cause the error type to be visible in the GraphQL server's "errors" array on the GraphQL schema. Learn more about
     Error.captureStackTrace(this, this.constructor);
   }
