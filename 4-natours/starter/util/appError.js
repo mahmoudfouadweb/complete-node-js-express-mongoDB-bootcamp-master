b6c3a86426bbd487abe6c52eab3806f3afa94efd
Mahmoud Fouad <mfapolice@gmail.com>
2023-04-10 06:07:54 +0200

    make Error class handler

---
+++ 4-natours/starter/util/appError.js
@@ -0,0 +1,11 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message); // Call the "Error" constructor, then pass in the message and statusCode as the arguments. This automatically attaches the prototype from the Error class
+    this.statusCode = statusCode;
+    this.status = statusCode.startWith('4') ? 'fail' : 'error';
+    this.isOperational = true; // This will cause the error type to be visible in the GraphQL server's "errors" array on the GraphQL schema. Learn more about
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
