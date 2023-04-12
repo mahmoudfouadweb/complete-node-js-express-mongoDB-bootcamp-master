65754baaa71495310b07debdf1714a5beec3d0f4
Mahmoud Fouad <mfapolice@gmail.com>
2023-04-12 04:23:29 +0200

    update

--- 4-natours/starter/controller/errorController.js
+++ 4-natours/starter/controller/errorController.js
@@ -1,5 +1,12 @@
+const AppError = require('../util/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invail ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
 // JSON FORMATTING FOR Client ERRORS
 const sendErrorDev = (err, res) => {
+  // Operational, trusted error: send message to client
   res.status(err.statusCode).json({
     status: err.status,
     error: err
@@ -8,6 +15,7 @@ const sendErrorDev = (err, res) => {
 
 // JSON FORMATTING FOR SERVER ERRORS
 const sendErrorProd = (err, res) => {
+  // Programming or other unknown error: don't leak error details to client. Show 500 internal server error.
   if (err.isOpreationError) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -16,18 +24,27 @@ const sendErrorProd = (err, res) => {
       message: err.message
     });
   } else {
-    res.statu(500).json({ status: 'error', error: err, message: err.message });
+    // 1) log error
+    console.log('Error 💥', err);
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Somthing went very werong!'
+    });
   }
 };
 
 // ERROR HANDLING FOR ALL ROUTES!!! NOT THE ONE THAT FAILS THE FIRST
 module.exports = (err, req, res, next) => {
-  err.statusCode = err.statuscode || 500;
+  err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    console.log(error.name);
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
